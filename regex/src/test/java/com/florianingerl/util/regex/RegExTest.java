158d1363ba6d6ee39f2b88d5f780fda12e81ae6c
Florian Ingerl <imelflorianingerl@gmail.com>
2016-10-06 13:00:41 +0200

    Added more tests for minimum and maximum length of a Pattern

--- regex/src/test/java/com/florianingerl/util/regex/RegExTest.java
+++ regex/src/test/java/com/florianingerl/util/regex/RegExTest.java
@@ -643,6 +643,16 @@ public class RegExTest {
 	
 	private static void minMaxLengthTest() throws Exception {
 		check("(a(?1)?z)", 2, false, -1);
+		check("(a(?1)??z)", 2, false, -1);
+		check("(a(?1)z|q)", 1, false, -1);
+		check("1(jT(\\<((?1)(,|(?=\\>)))+\\>)?)2", 4, false, -1 );
+		
+		check("(\\(([^()]+|(?1))*+\\))", 2, false, -1 );
+		check("\\b(([a-zA-Z])(?1)?(?<-2>\\2)|[a-zA-Z])\\b", 1, false, -1 );
+		
+		check("1(?<first>a\\((?<second>(?first)|[a-zA-Z]),(?second)\\))2", 8, false, -1 );
+		
+		check("(?:(a)|bc)(?(1)A|BBBBBB)", 2, true, 8 );
 		
 		report("minMaxLength");
 	}

6aac5157d1c249707940b7ad3fb80908374beb8b
fingerl <imelflorianingerl@gmail.com>
2017-07-28 15:01:45 +0200

    Modified test, whether java.util.regex needs less stacks!
    It seems as if the way, com.florianingerl.util.regex handles the
    repetition is bad!

--- regex/src/test/java/com/florianingerl/util/regex/tests/StackOverflowExceptionTest.java
+++ regex/src/test/java/com/florianingerl/util/regex/tests/StackOverflowExceptionTest.java
@@ -8,34 +8,39 @@ import com.florianingerl.util.regex.Matcher;
 import com.florianingerl.util.regex.Pattern;
 
 public class StackOverflowExceptionTest {
-
-	@Test
-	public void testRepetitionOfGroups() {
-		Pattern p = Pattern.compile("\"(\\\\.|[^\"])*\"");
-		StringBuilder input = new StringBuilder("\"");
-		for(int i=0; i < 1; ++i) {
-			for(int j=0; j < 1300; ++j) {
-				input.append("a");
-			}
-			input.append("\\\"");
-		}
-		input.append("\"");
-		assertTrue( p.matcher(input.toString()).matches() );
-		
-	}
 	
 	@Test
 	public void testRepetitionOfGroupsJavaUtilRegex() {
-		java.util.regex.Pattern p = java.util.regex.Pattern.compile("\"(\\\\.|[^\"])*\"");
-		StringBuilder input = new StringBuilder("\"");
-		for(int i=0; i < 1; ++i) {
-			for(int j=0; j < 1300; ++j) {
-				input.append("a");
-			}
-			input.append("\\\"");
+		java.util.regex.Pattern p = java.util.regex.Pattern.compile("(\\\\.|[^\"])*");
+		StringBuilder input = new StringBuilder();
+		for(int i=0; i < 8000; ++i) {
+			input.append("a");
 		}
-		input.append("\"");
 		assertTrue( p.matcher(input.toString()).matches() );
+	}
+	
+	@Test
+	public void testRepetitionOfGroups() {
+		Pattern p1 = Pattern.compile("(\\\\.|[^\"])*");
+		java.util.regex.Pattern p2 = java.util.regex.Pattern.compile(p1.pattern());
+		StringBuilder input = new StringBuilder();
+		boolean failed = false;
+		for(int i=0; i < 5000; ++i) {
+			input.append("a");
+			if(i <= 1000) continue;
+			System.out.println(i);
+			try {
+				assertTrue( p1.matcher(input.toString()).matches() );
+			} catch(StackOverflowError soe) {
+				soe.printStackTrace();
+				failed = true;
+			}
+			if(!failed) continue;
+			
+			if( p2.matcher(input.toString()).matches() )
+				assertTrue("com.florianingerl.util.regex needs more stacks!",false);
+			return;
+		}
 		
 	}
 

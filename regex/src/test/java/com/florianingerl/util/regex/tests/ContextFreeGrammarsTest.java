344e86c00447da1e6c6a37b0c7b2806bfb7e8180
florianingerl <imelflorianingerl@gmail.com>
2020-12-24 09:43:13 +0100

    Adds a few tests for context-free grammars!

--- regex/src/test/java/com/florianingerl/util/regex/tests/ContextFreeGrammarsTest.java
+++ regex/src/test/java/com/florianingerl/util/regex/tests/ContextFreeGrammarsTest.java
@@ -45,4 +45,50 @@ public class ContextFreeGrammarsTest {
 		assertTrue(p.matcher("bbcc").matches());
 	}
 
+	
+	@Test
+	public void test3() {
+		Pattern p = Pattern.compile("(?(DEFINE)(?<S>aa(?'S')b|c))(?'S')");
+		
+		assertTrue(p.matcher("aacb").matches() );
+		assertTrue(p.matcher("c").matches() );
+		assertTrue(p.matcher("aaaacbb").matches() );
+		assertFalse(p.matcher("acb").matches() );
+		assertFalse(p.matcher("aaacb").matches() );
+	}
+	
+	
+	@Test
+	public void test4() {
+		Pattern p = Pattern.compile("(?(DEFINE)(?<S>(?'C')b(?'B'))(?<B>b(?'B')|)(?<C>a(?'C')b|))(?'S')");
+	
+		assertTrue(p.matcher("b").matches() );
+		assertTrue(p.matcher("bb").matches() );
+		assertTrue(p.matcher("abb").matches() );
+		assertTrue(p.matcher("aabbbb").matches() );
+		assertFalse(p.matcher("ab").matches() );
+		assertFalse(p.matcher("aab").matches() );
+	}
+	
+	@Test
+	public void test5() {
+		Pattern p = Pattern.compile("(?(DEFINE)(?<S>(?'T')aba(?'T')|(?'T')bab(?'T'))(?<T>a(?'T')|b(?'T')|c(?'T')|))(?'S')");
+		
+		assertTrue(p.matcher("abacccc").matches() );
+		assertTrue(p.matcher("cccbab").matches() );
+		assertTrue(p.matcher("accabacab").matches() );
+		assertTrue(p.matcher("acbabccb").matches() );
+		assertFalse(p.matcher("abcabc").matches() );
+		
+	}
+	
+	@Test
+	public void test6() {
+		Pattern p = Pattern.compile("(?(DEFINE)(?<S>a(?'S')c|b(?'S')c|))(?'S')");
+		
+		assertTrue(p.matcher("abcc").matches() );
+		assertTrue(p.matcher("abaccc").matches() );
+		assertTrue(p.matcher("").matches() );
+		assertFalse(p.matcher("abccc").matches() );
+	}
 }
